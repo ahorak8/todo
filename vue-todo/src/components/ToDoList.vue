d3a09c7388b361edabdf5dcfe4294aea30e2b3fb
Aleksiel <aleksielhorak@gmail.com>
2020-03-03 15:40:13 +0200

    6: computed properties

--- vue-todo/src/components/ToDoList.vue
+++ vue-todo/src/components/ToDoList.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        <todo v-for="todo in todos" :todo="todo" :key="todo.id"></todo>
+        <div class="row">
+            <div class="columns large-6">
+                <h2>ToDo ({{ uncompletedTodos.length}})</h2>
+                <todo v-for="todo in uncompletedTodos" :todo="todo" :key="todo.id"></todo>
+            </div>
+                <div class="columns large-6">
+                    <h2>Completed ({{completedTodos.length}})</h2>
+                <todo v-for="todo in completedTodos" :todo="todo" :key="todo.id"></todo>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -15,6 +24,18 @@ export default {
     
         todo
 
+    },
+    computed: { // computed properties
+        completedTodos: function () { 
+            return this.todos.filter(function(todo){ // Wanted to filter all the todos that 
+                return todo.status == 1;            // have a status of 1 (completed)
+            })
+         },
+        uncompletedTodos: function () { 
+            return this.todos.filter(function(todo){ // Wanted to filter all the todos that 
+                return todo.status == 0;            // have a status of 0 (uncompleted)
+            })
+         }
     }
 }
 </script>
